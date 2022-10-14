62c46835622cf6ec3a216a88fd26a451bc533b79
markov-avl <andrewshasucks@gmail.com>
2022-10-14 21:16:07 +1000

    Задание 1 выполнено

---
+++ task_1.hs
@@ -0,0 +1,19 @@
+-- Определите функцию, вычисляющую двойной факториал,
+-- то есть произведение натуральных чисел, не превосходящих
+-- заданного числа и имеющих ту же четность.
+
+-- Например: 7!!=7⋅5⋅3⋅1,  8!!=8⋅6⋅4⋅2.
+
+-- Предполагается, что аргумент функции может принимать
+-- только неотрицательные значения.
+
+import Text.Printf
+
+doubleFactorial :: Int -> Int
+doubleFactorial 0 = 1
+doubleFactorial 1 = 1
+doubleFactorial n = n * doubleFactorial (n - 2)
+
+main = do
+    printf "7!! = %d\n" (doubleFactorial 7)
+    printf "8!! = %d\n" (doubleFactorial 8)
