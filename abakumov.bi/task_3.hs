e721d3e12701fac0b44beb88564335624fc558f9
CreatorOfEverything <annoyingfox159@gmail.com>
2022-10-22 21:09:49 +1000

    task 3 done

---
+++ abakumov.bi/task_3.hs
@@ -0,0 +1,11 @@
+fibonacci :: Integer -> Integer
+fibonacci x = helper x 0 1
+helper x a b = if x == 0 
+    then a 
+    else if x > 0 
+        then helper (x - 1) b (a + b) 
+        else helper (x + 1) b (a - b)
+
+main = do
+    print(fibonacci 100)
+    print(fibonacci (-100))
