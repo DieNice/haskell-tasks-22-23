3b22e1c87d06073e0e6bd5a3d66ec07e29119c8c
Dudoserovich <mr.egoryk@mail.ru>
2022-11-19 14:28:41 +1000

    fix: number < 0

--- khmelevsky.ed/task_4.hs
+++ khmelevsky.ed/task_4.hs
@@ -10,6 +10,7 @@ main = do
     print $ resultCost products
     where
         products = [
+                ("name_0", -3, 100.99),
                 ("name_1", 10, 100.99),
                 ("name_2", 5, 110),
                 ("name_3", 14, 75)
@@ -24,10 +25,10 @@ productName :: ProductList -> Name
 productName (name, _, _) = name
 
 productCount :: ProductList -> Count
-productCount (_, count, _) = count
+productCount (_, count, _) = if count >= 0 then count else error "Error: count < 0"
 
 productCost :: ProductList -> Cost
-productCost (_, _, cost) = cost
+productCost (_, _, cost) = if cost >= 0 then cost else error "Error: cost < 0"
 
 -- Проходимся по кортежам списка, вычисляем стоимость каждого товара и суммируем полученный список
 resultCost :: [ProductList] -> Float
