a6724f9eeab7d128b02d75a916d55fa35750babe
Transcendentall <60776766+Transcendentall@users.noreply.github.com>
2022-11-26 14:43:17 +1000

    Update 12.hs

--- 12.hs
+++ 12.hs
@@ -1,8 +1,6 @@
 sumncount :: Integer -> (Integer, Integer)
-sumncount x = (f1, f2)
+sumncount x = (f1x x 0, f2x x 0)
 	where 
-		f1 = f1x x 0
-		f2 = f2x x 0
 		f1x 0 acc = acc
 		f1x x acc = f1x (x `div` 10) (acc + 1) 
 		f2x 0 acc = acc
