4a87f1bcf7dacfa276587ed6d897a761ec4a14f2
Transcendentall <60776766+Transcendentall@users.noreply.github.com>
2022-12-10 14:21:11 +1000

    Update 4.hs

--- 4.hs
+++ 4.hs
@@ -2,7 +2,10 @@ data Tovar = Tovar String Integer Integer deriving (Show)
 price :: [Tovar] -> Integer
 
 price [] = 0
-price (x:xs) = oneTovarCount x * oneTovarValue x + price xs
+price (x:xs) | (oneTovarCount x >=  0) && (oneTovarValue x >=  0) = oneTovarCount x * oneTovarValue x + price xs
+             | oneTovarValue x <  0 = error "ERROR! Value < 0"
+             | oneTovarCount x <  0 = error "ERROR! Count < 0"
                 where oneTovarCount (Tovar name count value) = count
                       oneTovarValue (Tovar name count value) = value
+
 
