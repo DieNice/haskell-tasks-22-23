3cdef46335187504f41e7b969ccd3f8a20d96abb
CreatorOfEverything <annoyingfox159@gmail.com>
2022-12-10 13:52:43 +1000

    добавлено измерение работы

--- abakumov.bi/task_3.hs
+++ abakumov.bi/task_3.hs
@@ -1,3 +1,5 @@
+import Data.Time
+
 fibonacci :: Integer -> Integer
 fibonacci x = helper x 0 1
 helper x a b = if x == 0 
@@ -7,5 +9,8 @@ helper x a b = if x == 0
         else helper (x + 1) b (a - b)
 
 main = do
+    start <- getCurrentTime
     print(fibonacci 100)
+    stop <- getCurrentTime
+    print (diffUTCTime stop start)
     print(fibonacci (-100))
