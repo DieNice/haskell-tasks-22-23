7ac32bd36f60767a1b6308bd690ecebb529213b3
Transcendentall <60776766+Transcendentall@users.noreply.github.com>
2022-12-12 18:15:49 +1000

    Delete 8.hs

--- 8.hs
+++
@@ -1,5 +0,0 @@
-sort :: [Int] -> [Int]
-sort [] = []
-sort (p:xs) =     sort xs1  ++  [p]  ++  sort xs2
-                where  xs1 = [x | x <- xs, x > p] 
-                       xs2 = [x | x <- xs, x <= p]
