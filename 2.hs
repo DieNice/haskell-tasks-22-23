e7eba4d4675ced71942a454f59b41fb481729194
Transcendentall <60776766+Transcendentall@users.noreply.github.com>
2022-12-12 18:13:08 +1000

    Delete 2.hs

--- 2.hs
+++
@@ -1,7 +0,0 @@
-fibonacci::Integer -> Integer
-fibonacci (-2) = -1
-fibonacci (-1) = 1
-fibonacci 0 = 1
-fibonacci 1 = 1
-fibonacci n | n >  1  = fibonacci (n-1) + fibonacci (n-2)
-            | n < -2  = fibonacci (n+2) - fibonacci (n+1)
